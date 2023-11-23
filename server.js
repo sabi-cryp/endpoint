f68355158140abfb16532a6a21026c17233eaa56
sabrine kerchaoui <74612485+sabi-cryp@users.noreply.github.com>
2023-11-23 17:02:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,38 @@ const port = 30001;
 // Use bodyParser middleware to parse JSON requests
 app.use(bodyParser.json());
 
+// Map of clients to specific logic
+const clientLogicMap = {
+    client1: 'Logic for client1',
+    client2: 'Logic for client2',
+    client3: 'Logic for client3',
+};
+
 // GET endpoint
 app.get('/client/:nom_client', (req, res) => {
     const nomClient = req.params.nom_client;
-    res.send(`Endpoint for ${nomClient}: /api`);
+
+    // Check if the client has specific logic
+    if (clientLogicMap[nomClient]) {
+        res.send(`Endpoint for ${nomClient}: ${clientLogicMap[nomClient]}`);
+    } else {
+        res.status(404).send(`Client ${nomClient} not found`);
+    }
 });
 
 // POST endpoint
 app.post('/custom', (req, res) => {
     const nomClient = req.body.nom_client;
-    res.send(`Custom functionality for ${nomClient}`);
+
+    // Check if the client has specific logic
+    if (clientLogicMap[nomClient]) {
+        res.send(`Custom functionality for ${nomClient}: ${clientLogicMap[nomClient]}`);
+    } else {
+        res.status(404).send(`Client ${nomClient} not found`);
+    }
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://10.10.204.7:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
