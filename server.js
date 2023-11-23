2646116370042d1f5fcc9a051fb71be0c5d3bb29
sabrine kerchaoui <74612485+sabi-cryp@users.noreply.github.com>
2023-11-23 16:01:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 30001;  // Change the port to 30001
+const port = 30001;
 
-// Serve the index.html file at the /api endpoint
-app.get('/api', (req, res) => {
+// List of clients
+const clients = ['client1', 'client2', 'client3'];
+
+// Serve the index.html file at the root endpoint
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+// Serve an endpoint based on the client name in the URL
+app.get('/:nom_client', (req, res) => {
+    const nomClient = req.params.nom_client;
+
+    // Check if the client exists in the list
+    if (clients.includes(nomClient)) {
+        // Generate an endpoint based on the client name
+        const endpoint = `/${nomClient}/api`;
+
+        // Send a response with the generated endpoint
+        res.send(`Endpoint for ${nomClient}: ${endpoint}`);
+    } else {
+        // If the client is not in the list, send an error response
+        res.status(404).send(`Client ${nomClient} not found`);
+    }
 });
 
 // Start the server
