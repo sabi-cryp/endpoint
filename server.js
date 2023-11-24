4060b88e883acc7e774b85986d55d2979f584ae6
sabrine kerchaoui <74612485+sabi-cryp@users.noreply.github.com>
2023-11-24 08:28:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,34 @@
+// server.js
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
 const port = 30001;
 
-// Use bodyParser middleware to parse JSON requests
-app.use(bodyParser.json());
+// Exemple de données (simulant une base de données)
+const clients = [
+  { id: 1, name: 'Client1', email: 'client1@example.com' },
+  { id: 2, name: 'Client2', email: 'client2@example.com' },
+  // Ajoutez d'autres clients au besoin
+];
 
-// Map of clients to specific logic
-const clientLogicMap = {
-    client1: 'Logic for client1',
-    client2: 'Logic for client2',
-    client3: 'Logic for client3',
-};
+app.get('/client/:clientName', (req, res) => {
+  const clientName = req.params.clientName;
 
-// GET endpoint
-app.get('/client/:nom_client', (req, res) => {
-    const nomClient = req.params.nom_client;
+  // Recherche du client par nom
+  const client = clients.find(c => c.name === clientName);
 
-    // Check if the client has specific logic
-    if (clientLogicMap[nomClient]) {
-        res.send(`Endpoint for ${nomClient}: ${clientLogicMap[nomClient]}`);
-    } else {
-        res.status(404).send(`Client ${nomClient} not found`);
-    }
+  if (client) {
+    // Si le client est trouvé, renvoie ses informations
+    res.json({
+      id: client.id,
+      name: client.name,
+      email: client.email
+    });
+  } else {
+    // Si le client n'est pas trouvé, renvoie une réponse appropriée
+    res.status(404).json({ error: 'Client non trouvé' });
+  }
 });
 
-// POST endpoint
-app.post('/custom', (req, res) => {
-    const nomClient = req.body.nom_client;
-
-    // Check if the client has specific logic
-    if (clientLogicMap[nomClient]) {
-        res.send(`Custom functionality for ${nomClient}: ${clientLogicMap[nomClient]}`);
-    } else {
-        res.status(404).send(`Client ${nomClient} not found`);
-    }
-});
-
-// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Le service web est en écoute sur le port ${port}`);
 });
